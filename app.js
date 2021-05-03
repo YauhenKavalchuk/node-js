d47b875441d0e8ab16ba54beb4d12957a1a0914c
Yauhen <silent_control@tut.by>
2021-05-03 17:00:45 +0300

    Create Base Routing

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   console.log('Server request');
 
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'text/html');
 
-  const data = JSON.stringify([
-    { name: 'Tommy', age: 35 },
-    { name: 'Arthur', age: 40 },
-  ]);
-  
-  // res.write('<head><link rel="stylesheet" href="#"></head>');
-  // res.write('<h1>Hello world!</h1>');
-  // res.write('<p>My name is Yauhen</p>');
+  const createPath = (page) => `${__dirname}/views/${page}.html`;
+  let path = '';
 
+  switch(req.url) {
+    case '/':
+    case '/home':
+    case '/index.html':
+      path = createPath('index');
+      res.statusCode = 200;
+      break;
+    case '/about-us':
+      res.statusCode = 301;
+      res.setHeader('Location', '/contacts');
+      res.end();
+      break;
+    case '/contacts':
+      path = createPath('contacts');
+      res.statusCode = 200;
+      break;
+    default:
+      path = createPath('error');
+      res.statusCode = 404;
+      break;
+  }
 
-  res.end(data);
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.statusCode = 500;
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(PORT, 'localhost', (error) => {
