506bdf442450e89833cb7fa66452d58a3277157d
Yauhen <silent_control@tut.by>
2021-05-07 08:57:02 +0300

    Handling POST requests

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
+app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
@@ -35,18 +36,47 @@ app.get('/contacts', (req, res) => {
 });
 
 app.get('/posts/:id', (req, res) => {
+  const post = {
+    id: '1', 
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
+    title: 'Post title',
+    data: '05.05.2021',
+    author: 'Yauhen',
+  };
   const title = 'Post';
-  res.render(createPath('post'), { title });
+  res.render(createPath('post'), { post, title });
 });
 
 app.get('/posts', (req, res) => {
+  const posts = [
+    {
+      id: '1', 
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
+      title: 'Post title',
+      data: '05.05.2021',
+      author: 'Yauhen',
+    },
+  ];
   const title = 'Posts';
-  res.render(createPath('posts'), { title });
+  res.render(createPath('posts'), { posts, title });
 });
 
 app.get('/add-post', (req, res) => {
   const title = 'Add Post';
   res.render(createPath('add-post'), { title });
+});
+
+app.post('/add-post', (req, res) => {
+  //res.send(req.body);
+  const { title, author, text } = req.body;
+  const post = {
+    id: new Date(),
+    data: (new Date()).toLocaleDateString(),
+    title,
+    author,
+    text,
+  }
+  res.render(createPath('post'), { post, title });
 });
 
 app.use((req, res) => {
