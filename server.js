29068d0f0b2b1bd84eb9a23e6715cb2cea03a5aa
Yauhen <silent_control@tut.by>
2021-05-09 19:53:10 +0300

    Delete & Update Requests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Post = require('./models/post');
 const Contact = require('./models/contact');
 
@@ -29,6 +30,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 app.use(express.static('styles'));
 
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
   const title = 'Home';
   res.render(createPath('index'), { title });
@@ -50,6 +53,41 @@ app.get('/posts/:id', (req, res) => {
   Post
     .findById(req.params.id)
     .then(post => res.render(createPath('post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('edit-post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+  Post
+    .findByIdAndUpdate(req.params.id, { title, author, text })
+    .then((result) => res.redirect(`/posts/${id}`))
     .catch((error) => {
       console.log(error);
       res.render(createPath('error'), { title: 'Error' });
