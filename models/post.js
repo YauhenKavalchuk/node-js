e794cb259262812f2cc57a58a2881715e7de95e4
Yauhen <silent_control@tut.by>
2021-05-09 09:12:40 +0300

    MongoDB Integration

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true });
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 const PORT = 3000;
+const db = 'mongodb+srv://Yauhen:Pass321@cluster0.islwq.mongodb.net/node-blog?retryWrites=true&w=majority';
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log('Connected to DB'))
+  .catch((error) => console.log(error));
 
 const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
@@ -67,16 +75,15 @@ app.get('/add-post', (req, res) => {
 });
 
 app.post('/add-post', (req, res) => {
-  //res.send(req.body);
   const { title, author, text } = req.body;
-  const post = {
-    id: new Date(),
-    data: (new Date()).toLocaleDateString(),
-    title,
-    author,
-    text,
-  }
-  res.render(createPath('post'), { post, title });
+  const post = new Post({ title, author, text });
+  post
+    .save()
+    .then((result) => res.send(result))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
 });
 
 app.use((req, res) => {
