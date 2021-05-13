8169c31affc88aea773788742f1bb44e135f92e9
Yauhen <silent_control@tut.by>
2021-05-13 19:01:52 +0300

    Add chalk remove env

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ require('dotenv/config');
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const chalk = require('chalk');
 const methodOverride = require('method-override');
 const postRoutes = require('./routes/post-routes');
 const postApiRoutes = require('./routes/api-post-routes');
 const contactRoutes = require('./routes/contact-routes');
 const createPath = require('./helpers/create-path');
+
+const errorMsg = chalk.bgKeyword('white').redBright;
+const successMsg = chalk.bgKeyword('green').white;
 
 const app = express();
 
@@ -14,11 +18,13 @@ app.set('view engine', 'ejs');
 
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((res) => console.log('Connected to DB'))
-  .catch((error) => console.log(error));
+  .then((res) => console.log(successMsg('Connected to DB')))
+  .catch((error) => console.log(errorMsg(error)));
 
 app.listen(process.env.PORT, (error) => {
-  error ? console.log(error) : console.log(`listening port ${process.env.PORT}`);
+  error 
+    ? console.log(errorMsg(error)) 
+    : console.log(successMsg(`listening port ${process.env.PORT}`));
 });
 
 app.use(express.urlencoded({ extended: false }));
